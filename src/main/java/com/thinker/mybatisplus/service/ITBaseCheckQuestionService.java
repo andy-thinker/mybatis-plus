03a0b5cc0036cf7713fd756808cddbdd9877f563
andy <andy_thinker@yeah.net>
2019-09-17 22:02:04 +0800

    代码生成，初始化

---
+++ src/main/java/com/thinker/mybatisplus/service/ITBaseCheckQuestionService.java
@@ -0,0 +1,17 @@
+package com.thinker.mybatisplus.service;
+
+import com.thinker.mybatisplus.entity.TBaseCheckQuestion;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+ * <p>
+ * 安全检查-问题 服务类
+ * </p>
+ *
+ * @author andy
+	@email:andy_thinker@yeah.net
+ * @since 2019-09-17
+ */
+public interface ITBaseCheckQuestionService extends IService<TBaseCheckQuestion> {
+
+}
